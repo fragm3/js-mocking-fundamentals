f51fe1d272dbd6905c19d4bd852e57b192e6bef6
Shubham Gupta <shubhamg@outlook.com>
2021-05-19 03:28:35 +0530

    refactor

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const App = ({ onClick }) => (
-  <div>
-    <div>Hello world React!</div>
-    <button onClick={onClick}>btn</button>
-  </div>
-);
--- src/__tests__/app.spec.js
+++ src/__tests__/app.spec.js
@@ -3,14 +3,23 @@ import { render, screen } from '@testing-library/react';
 //import userEvent from '@testing-library/user-event';
 import { simpleFireEvent } from '../rtl/simpleFireEvent';
 import userEvent from '../rtl/userEvent';
-import { App } from '../App';
 
-describe('App Component', function () {
-  it('should have hello world message', function () {
-    const onClickFn = jest.fn();
-    render(<App onClick={onClickFn} />);
-    const button = screen.getByText(/btn/i);
-    userEvent.click(button);
-    expect(onClickFn).toHaveBeenCalledTimes(1);
-  });
+const App = ({ onClick }) => (
+  <div>
+    <div>Hello world React!</div>
+    <button onClick={onClick}>btn</button>
+  </div>
+);
+
+test('should have hello world message', function () {
+  const onClickFn = jest.fn();
+
+  render(<App onClick={onClickFn} />);
+
+  const button = screen.getByText(/btn/i);
+
+  // simpleFireEvent(button, 'click');
+  userEvent.click(button);
+
+  expect(onClickFn).toHaveBeenCalledTimes(1);
 });
